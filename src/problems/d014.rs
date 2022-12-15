d8bbdad3176b64db4a94513644e7527388bc67d4
c-to-the-l <39165068+c-to-the-l@users.noreply.github.com>
2022-12-15 00:21:29 +0000

    day 14

--- src/problems/d014.rs
+++ src/problems/d014.rs
@@ -1,19 +1,106 @@
+use std::collections::HashSet;
+
 pub struct Day {
-    p1: i64,
-    p2: i64,
+    p1: usize,
+    p2: usize,
+    input: String,
+    rock: HashSet<(i32, i32)>,
+    p1s: HashSet<(i32, i32)>,
+    p2s: HashSet<(i32, i32)>,
+}
+
+impl Day {
+    fn move_grain(&self, grain: (i32, i32)) -> Option<(i32, i32)> {
+        if !self.p1s.contains(&(grain.0, grain.1 + 1)) {
+            Some((grain.0, grain.1 + 1))
+        } else if !self.p1s.contains(&(grain.0 - 1, grain.1 + 1)) {
+            Some((grain.0 - 1, grain.1 + 1))
+        } else if !self.p1s.contains(&(grain.0 + 1, grain.1 + 1)) {
+            Some((grain.0 + 1, grain.1 + 1))
+        } else {
+            None
+        }
+    }
+    fn move_grain_p2(&self, grain: (i32, i32)) -> Option<(i32, i32)> {
+        if !self.p2s.contains(&(grain.0, grain.1 + 1)) {
+            Some((grain.0, grain.1 + 1))
+        } else if !self.p2s.contains(&(grain.0 - 1, grain.1 + 1)) {
+            Some((grain.0 - 1, grain.1 + 1))
+        } else if !self.p2s.contains(&(grain.0 + 1, grain.1 + 1)) {
+            Some((grain.0 + 1, grain.1 + 1))
+        } else {
+            None
+        }
+    }
 }
 
 impl crate::Problem for Day {
     const YEAR: u32 = crate::YEAR;
     const DAY: u32 = 14;
 
-    fn new(_input: String) -> Self {
-        Self { p1: 0, p2: 0 }
+    fn new(input: String) -> Self {
+        Self {
+            p1: 0,
+            p2: 0,
+            input,
+            rock: HashSet::new(),
+            p1s: HashSet::new(),
+            p2s: HashSet::new(),
+        }
     }
 
-    fn do_p1(&mut self) {}
+    fn do_p1(&mut self) {
+        for line in self.input.lines() {
+            for (prev, next) in line.split(" -> ").zip(line.split(" -> ").skip(1)) {
+                let ((prev_l, prev_r), (next_l, next_r)) =
+                    (prev.split_once(',').unwrap(), next.split_once(',').unwrap());
+                let (prev_l, prev_r, next_l, next_r) = (
+                    prev_l.parse::<i32>().unwrap(),
+                    prev_r.parse::<i32>().unwrap(),
+                    next_l.parse::<i32>().unwrap(),
+                    next_r.parse::<i32>().unwrap(),
+                );
+                for x in prev_l.min(next_l)..=prev_l.max(next_l) {
+                    for y in prev_r.min(next_r)..=prev_r.max(next_r) {
+                        self.rock.insert((x, y));
+                    }
+                }
+            }
+        }
+        let lim = *self.rock.iter().map(|(_, a)| a).max().unwrap();
+        self.p1s = self.rock.clone();
 
-    fn do_p2(&mut self) {}
+        'outer: loop {
+            let mut grain = (500i32, 0i32);
+            while let Some(new_g) = self.move_grain(grain) {
+                grain = new_g;
+                if grain.1 > lim {
+                    break 'outer;
+                }
+            }
+            self.p1 += 1;
+            self.p1s.insert(grain);
+        }
+    }
+
+    fn do_p2(&mut self) {
+        self.p2s = self.rock.clone();
+        let lim = *self.rock.iter().map(|(_, a)| a).max().unwrap() + 1;
+        loop {
+            let mut grain = (500i32, 0i32);
+            while let Some(new_g) = self.move_grain_p2(grain) {
+                grain = new_g;
+                if grain.1 >= lim {
+                    break;
+                }
+            }
+            self.p2 += 1;
+            if grain == (500, 0) {
+                break;
+            }
+            self.p2s.insert(grain);
+        }
+    }
 
     fn p1_result(&self) -> String {
         format!("{}", self.p1)
