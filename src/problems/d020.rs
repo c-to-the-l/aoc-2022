9650021af0e0d6a163f667e4675a61354fd615d1
c-to-the-l <39165068+c-to-the-l@users.noreply.github.com>
2022-12-21 19:07:24 +0000

    days 20 and 21

--- src/problems/d020.rs
+++ src/problems/d020.rs
@@ -1,19 +1,88 @@
 pub struct Day {
     p1: i64,
     p2: i64,
+    input: String,
+    stack: Vec<i64>,
+    stack_p2: Vec<i64>,
+
+    indices: Vec<usize>,
+    indices_p2: Vec<usize>,
+}
+
+fn mov<T>(v: &mut Vec<T>, idx: i64, by: i64) {
+    let len = v.len() as i64 - 1;
+    if by % len == 0 {
+        return;
+    }
+    let new_idx = (idx + (by % len) + len) % len;
+    // println!("{} {} {} {}", idx, e_by, by, len);
+    let v_r = v.remove(idx as usize);
+    v.insert(new_idx as usize, v_r);
 }
 
 impl crate::Problem for Day {
     const YEAR: u32 = crate::YEAR;
     const DAY: u32 = 20;
 
-    fn new(_input: String) -> Self {
-        Self { p1: 0, p2: 0 }
+    fn new(input: String) -> Self {
+        Self {
+            p1: 0,
+            p2: 0,
+            input,
+            stack: Vec::new(),
+            stack_p2: Vec::new(),
+            indices: Vec::new(),
+            indices_p2: Vec::new(),
+        }
     }
 
-    fn do_p1(&mut self) {}
+    fn do_p1(&mut self) {
+        let mut parse = nom::multi::separated_list1::<_, _, _, nom::error::Error<_>, _, _>(
+            nom::character::complete::char('\n'),
+            nom::character::complete::i64,
+        );
+        self.stack = parse(self.input.as_str()).unwrap().1;
+        self.indices = (0..self.stack.len()).collect();
+        for i in 0..self.stack.len() {
+            let idx = self.indices.iter().position(|v| *v == i).unwrap();
+            let by = self.stack[idx];
+            mov(&mut self.indices, idx as i64, by);
+            mov(&mut self.stack, idx as i64, by);
+        }
 
-    fn do_p2(&mut self) {}
+        let z_idx = self.stack.iter().position(|v| *v == 0).unwrap();
+        let len = self.stack.len();
+        self.p1 = self.stack[(1000 + z_idx) % len]
+            + self.stack[(2000 + z_idx) % len]
+            + self.stack[(3000 + z_idx) % len];
+    }
+
+    fn do_p2(&mut self) {
+        let mut parse = nom::multi::separated_list1::<_, _, _, nom::error::Error<_>, _, _>(
+            nom::character::complete::char('\n'),
+            nom::character::complete::i64,
+        );
+        self.stack_p2 = parse(self.input.as_str())
+            .unwrap()
+            .1
+            .iter()
+            .map(|v| v * 811589153)
+            .collect();
+        self.indices_p2 = (0..self.stack_p2.len()).collect();
+        for _ in 0..10 {
+            for i in 0..self.stack_p2.len() {
+                let idx = self.indices_p2.iter().position(|v| *v == i).unwrap();
+                let by = self.stack_p2[idx];
+                mov(&mut self.indices_p2, idx as i64, by);
+                mov(&mut self.stack_p2, idx as i64, by);
+            }
+        }
+        let z_idx = self.stack_p2.iter().position(|v| *v == 0).unwrap();
+        let len = self.stack_p2.len();
+        self.p2 = self.stack_p2[(1000 + z_idx) % len]
+            + self.stack_p2[(2000 + z_idx) % len]
+            + self.stack_p2[(3000 + z_idx) % len];
+    }
 
     fn p1_result(&self) -> String {
         format!("{}", self.p1)
--- src/problems/d021.rs
+++ src/problems/d021.rs
@@ -1,19 +1,190 @@
+use std::collections::HashMap;
+
+fn make_idx(s: &str) -> usize {
+    s.as_bytes()
+        .iter()
+        .enumerate()
+        .map(|(j, v)| 100usize.pow(j as u32) * (v - ('a' as u8)) as usize)
+        .sum()
+}
+
+#[derive(Debug)]
+enum Op {
+    Add,
+    Sub,
+    Div,
+    Mul,
+}
+
+impl Op {
+    fn from_char(c: char) -> Self {
+        use Op::*;
+        match c {
+            '-' => Sub,
+            '/' => Div,
+            '+' => Add,
+            '*' => Mul,
+            x => panic!("Unexpected Operator {}", x),
+        }
+    }
+    fn operate(&self, l: i64, r: i64) -> i64 {
+        use Op::*;
+        match self {
+            Add => l + r,
+            Sub => l - r,
+            Div => l / r,
+            Mul => l * r,
+        }
+    }
+
+    fn inputof_r(&self, l: i64, res: i64) -> i64 {
+        use Op::*;
+        match self {
+            Add => res - l,
+            Sub => l - res,
+            Div => l / res,
+            Mul => res / l,
+        }
+    }
+
+    fn inputof_l(&self, r: i64, res: i64) -> i64 {
+        use Op::*;
+        match self {
+            Add => res - r,
+            Sub => res + r,
+            Div => res * r,
+            Mul => res / r,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Monke {
+    Number(i64),
+    Do(Op, usize, usize),
+}
+
+impl Monke {
+    fn from_str(s: &str) -> (usize, Self) {
+        let mut parse = nom::sequence::tuple::<_, _, nom::error::Error<_>, _>((
+            nom::bytes::complete::take(4usize),
+            nom::bytes::complete::tag(": "),
+            nom::combinator::opt(nom::character::complete::i64),
+            nom::combinator::opt(nom::sequence::tuple((
+                nom::bytes::complete::take(4usize),
+                nom::bytes::complete::tag(" "),
+                nom::character::complete::one_of::<_, _, _>("-+/*"),
+                nom::bytes::complete::tag(" "),
+                nom::bytes::complete::take(4usize),
+            ))),
+        ));
+
+        let (_, (name, _, maybe_const, maybe_op)) = parse(s).unwrap();
+
+        if let Some(i) = maybe_const {
+            (make_idx(name), Self::Number(i))
+        } else if let Some((l, _, op, _, r)) = maybe_op {
+            (
+                make_idx(name),
+                Self::Do(Op::from_char(op), make_idx(l), make_idx(r)),
+            )
+        } else {
+            panic!("Doesn't appear to have a const or operator: {}", s);
+        }
+    }
+}
+
+trait MonkeAdd {
+    fn get_monke(&self, idx: usize) -> i64;
+
+    fn get_roots(&self) -> (usize, usize);
+    fn needs_idx(&self, idx: usize, needs: usize) -> bool;
+    fn unknown_idx(&self, here: usize, target: usize, input: i64) -> i64;
+}
+
+impl MonkeAdd for HashMap<usize, Monke> {
+    fn get_monke(&self, idx: usize) -> i64 {
+        match self.get(&idx) {
+            Some(Monke::Number(i)) => *i,
+            Some(Monke::Do(op, l, r)) => op.operate(self.get_monke(*l), self.get_monke(*r)),
+            None => panic!("Unexpected monke in bagging area: {}", idx),
+        }
+    }
+
+    fn get_roots(&self) -> (usize, usize) {
+        match self.get(&make_idx("root")) {
+            Some(Monke::Do(_, l, r)) => (*l, *r),
+            x => panic!("Bad root: {:?}", x),
+        }
+    }
+
+    fn needs_idx(&self, idx: usize, needs: usize) -> bool {
+        match self.get(&idx) {
+            Some(Monke::Number(_i)) => idx == needs,
+            Some(Monke::Do(_, l, r)) => {
+                (idx == needs) | self.needs_idx(*l, needs) | self.needs_idx(*r, needs)
+            }
+            None => panic!("Unexpected monke in bagging area: {}", idx),
+        }
+    }
+
+    fn unknown_idx(&self, here: usize, target: usize, input: i64) -> i64 {
+        if here == target {
+            return input;
+        }
+        match self.get(&here) {
+            Some(Monke::Do(op, l, r)) => {
+                if self.needs_idx(*l, target) {
+                    self.unknown_idx(*l, target, op.inputof_l(self.get_monke(*r), input))
+                } else {
+                    self.unknown_idx(*r, target, op.inputof_r(self.get_monke(*l), input))
+                }
+            }
+            x => panic!("Logic Error: Called unknown_idx on {:?}", x),
+        }
+    }
+}
+
 pub struct Day {
     p1: i64,
     p2: i64,
+    input: String,
+    monkes: HashMap<usize, Monke>,
 }
 
 impl crate::Problem for Day {
     const YEAR: u32 = crate::YEAR;
     const DAY: u32 = 21;
 
-    fn new(_input: String) -> Self {
-        Self { p1: 0, p2: 0 }
+    fn new(input: String) -> Self {
+        Self {
+            p1: 0,
+            p2: 0,
+            input,
+            monkes: HashMap::new(),
+        }
     }
 
-    fn do_p1(&mut self) {}
+    fn do_p1(&mut self) {
+        self.monkes = self.input.lines().map(Monke::from_str).collect();
+        // assert_eq!(self.monkes.len(), self.input.lines().count(), "Problem with key generator - two monkey names result in the same index");
+        // println!("{:#?}", self.monkes);
+        self.p1 = self.monkes.get_monke(make_idx("root"))
+    }
 
-    fn do_p2(&mut self) {}
+    fn do_p2(&mut self) {
+        let (rootl, rootr) = self.monkes.get_roots();
+        let (humn_head, target) = if self.monkes.needs_idx(rootl, make_idx("humn")) {
+            (rootl, self.monkes.get_monke(rootr))
+        } else {
+            assert!(
+                self.monkes.needs_idx(rootr, make_idx("humn")),
+                "How does neither branch of the tree require the humn input?"
+            );
+            (rootr, self.monkes.get_monke(rootl))
+        };
+        self.p2 = self.monkes.unknown_idx(humn_head, make_idx("humn"), target)
+    }
 
     fn p1_result(&self) -> String {
         format!("{}", self.p1)
